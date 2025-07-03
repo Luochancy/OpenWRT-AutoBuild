720bdc5db468772b109857ac45af58d7e3baa340
洛寒兮Luochancy <86495643+Luochancy@users.noreply.github.com>
2025-07-04 01:56:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 1. 修改默认IP为 10.0.100.1
-sed -i 's/10.0.100.1/g' package/base-files/files/bin/config_generate
