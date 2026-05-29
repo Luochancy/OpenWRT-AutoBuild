03c354d41f520944507f30275bacc4eff5dd39f6
Luochancy <me@luochancy.com>
2026-05-29 22:07:39 +0800

    Del: nikki

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,12 +11,11 @@
 #
 
 # ImmortalWrt v24.10.5 provides most requested packages in its official feeds.
-# Extra feeds cover iStore, NAS/iStore apps, and Nikki.
+# Extra feeds cover iStore and NAS/iStore apps.
 echo >>feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >>feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
-echo 'src-git nas_packages https://github.com/linkease/nas-packages.git;main' >>feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
 echo 'src-git app_meta https://github.com/linkease/openwrt-app-meta.git;main' >>feeds.conf.default
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >>feeds.conf.default
 
-echo "Using ImmortalWrt v24.10.5 with iStore, NAS app, OpenFog app-meta, and Nikki feeds."
+echo "Using ImmortalWrt v24.10.5 with iStore, NAS app, and OpenFog app-meta feeds."
